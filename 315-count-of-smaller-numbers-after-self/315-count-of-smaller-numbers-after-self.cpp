607526db67bd7f0c85179138e66d1d1c3f679c9f
CodeBulletin <48746746+CodeBulletin@users.noreply.github.com>
2022-07-23 13:04:16 +0530

    Time: 1154 ms (29.58%), Space: 216.2 MB (28.17%) - LeetHub

--- 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.cpp
+++ 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void mergeSort(vector<pair<int,int>>&v, vector<int>&count, int l, int m , int r){
+    void merge(vector<pair<int,int>>&v, vector<int>&count, int l, int m , int r){
         vector<pair<int,int>>t(r-l+1);
         int i = l, j = m+1, k = 0;
         while(i <= m && j <= r){
@@ -26,12 +26,12 @@ public:
         }
     }
     
-    void solve(vector<pair<int,int>>&v, vector<int>&count, int l, int r){
+    void mergeSort(vector<pair<int,int>>&v, vector<int>&count, int l, int r){
         if(l < r){
             int m = l+(r-l)/2;
-            solve(v, count, l, m);
-            solve(v, count, m+1, r);
-            mergeSort(v, count,l , m , r);
+            mergeSort(v, count, l, m);
+            mergeSort(v, count, m+1, r);
+            merge(v, count,l , m , r);
         }
     }
     
@@ -42,7 +42,7 @@ public:
         for(int i = 0; i < n; i++){
             num.push_back({nums[i],i});
         }
-        solve(num,count,0,n-1);
+        mergeSort(num,count,0,n-1);
         return count;
     }
 };
