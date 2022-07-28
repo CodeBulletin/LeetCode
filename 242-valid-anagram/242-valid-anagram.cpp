e201f64624dd8b3283a48fca58cc1a3b8f9c532c
CodeBulletin <48746746+CodeBulletin@users.noreply.github.com>
2022-07-28 15:00:59 +0530

    Time: 17 ms (44.89%), Space: 7.3 MB (45.59%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -5,9 +5,6 @@ public:
         sort(s.begin(), s.end());
         sort(t.begin(), t.end());
         
-        for(int i = 0; i < s.size(); i++) {
-            if(s[i] != t[i]) return false;
-        }
-        return true;
+        return s == t;
     }
 };
