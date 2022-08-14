b0c1862e84ed895f919188e35b40dc198833b0f5
Bhavesh Malhotra <48746746+CodeBulletin@users.noreply.github.com>
2022-08-14 20:40:14 +0530

    Time: 20 ms (88.86%), Space: 19.5 MB (59.43%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -3,9 +3,11 @@ public:
     int majorityElement(vector<int>& nums) {
         int count = 0;
         int candidate = -1;
+        int p = nums.size() / 2;
         for(int& num: nums) {
             if(count == 0)
                 candidate = num;
+            else if (count > p) return candidate;
             count += num == candidate ? 1 : -1;
         }
         return candidate;
