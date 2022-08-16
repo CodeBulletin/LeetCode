2870cbc50626983a3390c9ca87dbab8b21a1e140
Bhavesh Malhotra <48746746+CodeBulletin@users.noreply.github.com>
2022-08-16 19:27:36 +0530

    Time: 5 ms (22.38%), Space: 6.1 MB (17.71%) - LeetHub

---
+++ 1780-check-if-number-is-a-sum-of-powers-of-three/1780-check-if-number-is-a-sum-of-powers-of-three.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool checkPowersOfThree(int n) {
+        int k = log2((double)n) / log2(3.0);
+        while(k >= 0) {
+            n = n - pow(3, k);
+            if(n == 0) break;
+            int k2 = log2((double)n) / log2(3.0);
+            if(k2 >= k) return false;
+            k = k2;
+        }
+        return n == 0;
+    }
+};
