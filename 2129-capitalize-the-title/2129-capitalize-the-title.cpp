de974c1e0513f20897fae180bf2b2b4b156b194c
Bhavesh Malhotra <48746746+CodeBulletin@users.noreply.github.com>
2022-08-17 17:51:45 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (45.72%) - LeetHub

--- 2129-capitalize-the-title/2129-capitalize-the-title.cpp
+++ 2129-capitalize-the-title/2129-capitalize-the-title.cpp
@@ -2,12 +2,13 @@ class Solution {
 public:
     string capitalizeTitle(string& title) {
         transform(title.begin(), title.end(), title.begin(), ::tolower);
-        
         if(title.size() >= 3 and (title[1] != ' ' and title[2] != ' '))
             title[0] = toupper(title[0]);
         for(int i = 1; i < title.size(); i++) {
-            if(title[i] == ' ' and (i+3 < title.size() and (title[i+3] != ' ' and title[i+2] != ' ')))
+            if(title[i] == ' ' and (i+3 < title.size() and (title[i+3] != ' ' and title[i+2] != ' '))) {
                 title[i+1] = toupper(title[i+1]);
+                i+=2;
+            }
         }
         return title;
     }
