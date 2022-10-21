666d355ba7395e3a619354a25403e6758c5ec45a
Bhavesh Malhotra <48746746+CodeBulletin@users.noreply.github.com>
2022-10-21 18:41:15 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (42.51%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.cpp
+++ 0771-jewels-and-stones/0771-jewels-and-stones.cpp
@@ -3,12 +3,12 @@ public:
     int numJewelsInStones(string& jewels, string& stones) {
         unordered_map<char, int> jcount;
         
-        for(char& i : jewels) jcount[i] = 0;
+        for(const char& i : jewels) jcount[i] = 0;
         
-        for(char& i: stones) if(jcount.find(i) != jcount.end()) jcount[i] += 1;
+        for(const char& i: stones) if(jcount.find(i) != jcount.end()) jcount[i] += 1;
         
         int j = 0;
-        for(auto& i : jcount) {
+        for(const auto& i : jcount) {
             j += i.second;
         }
         return j;
