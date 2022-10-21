d601759c37a36e98998fd917298815f92e9bc198
Bhavesh Malhotra <48746746+CodeBulletin@users.noreply.github.com>
2022-10-21 18:52:27 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (8.19%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,8 +1,9 @@
-const float sqrt5 = sqrt(5);
-const float phi = (sqrt5 + 1) / 2;
+
 class Solution {
+    const float sqrt5 = sqrt(5);
+    const float phi = (sqrt5 + 1) / 2;
 public:
-    int fib(int& n) { 
+    int fib(const int& n) { 
         return round(pow(phi, n) / sqrt5);
     }
 };
