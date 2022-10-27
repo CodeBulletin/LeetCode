c7e8b48f54205551502033bc182cb8e6a84812ad
Bhavesh Malhotra <48746746+CodeBulletin@users.noreply.github.com>
2022-10-27 18:37:52 +0530

    Time: 380 ms (23.61%), Space: 10 MB (52.78%) - LeetHub

--- 0835-image-overlap/0835-image-overlap.cpp
+++ 0835-image-overlap/0835-image-overlap.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    int convolute(const vector<int>& a, const vector<vector<int>>& kernel, int x) {
-        int n = x - kernel.size();
+    int convolute(const vector<int>& a, const vector<int>& kernel, int x, int y) {
+        int n = x - y;
         
         int max = 0;
         
         for(int i = 0; i <= n ; i++) {
             for(int j = 0; j <= n; j++) {
                 int sum  = 0;
-                for(int k = 0; k < kernel.size(); k++) {
-                    for(int l = 0; l < kernel.size(); l++) {
-                        sum += a[(i + k) * x + j + l] * kernel[k][l];
+                for(int k = 0; k < y; k++) {
+                    for(int l = 0; l < y; l++) {
+                        sum += a[(i + k) * x + j + l] * kernel[k * y + l];
                     }
                 }
                 if(sum >= max) max = sum;
@@ -21,13 +21,18 @@ public:
     int largestOverlap(vector<vector<int>>& img1, vector<vector<int>>& img2) {
         int n = img1.size();
         int x = n * 3 - 2;
-        vector<int> im2(x * x, 0);
+        vector<int> im2(x * x, 0), im1(n * n);
         for(int i = 0; i < img2.size(); i++) {
             for(int j = 0; j < img2.size(); j++) {
                 im2[(i + n - 1) * x + j + n - 1] = img2[i][j];
             } 
         }
+        for(int i = 0; i < img1.size(); i++) {
+            for(int j = 0; j < img1.size(); j++) {
+                im1[i * n + j] = img1[i][j];
+            } 
+        }
         
-        return convolute(im2, img1, x);
+        return convolute(im2, im1, x, n);
     }
 };
