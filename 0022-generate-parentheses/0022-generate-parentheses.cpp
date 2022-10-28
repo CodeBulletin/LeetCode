316483b1a2853b34296badb1088031865c9ff711
Bhavesh Malhotra <48746746+CodeBulletin@users.noreply.github.com>
2022-10-28 16:46:29 +0530

    Time: 4 ms (76.87%), Space: 11.4 MB (87.19%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,28 @@
+class Solution {
+    static vector<vector<string>> mem;
+    static int h;
+public:
+    void gen(vector<string>& ans, string& str, int open, int close, int max) {
+        if (str.size() == max * 2) {
+            ans.push_back(str);
+            return;
+        }
+
+        if (open < max) {
+            str += '(';
+            gen(ans, str, open+1, close, max);
+            str.resize(str.size() - 1);
+        }
+        if (close < open) {
+            str.append(")");
+            gen(ans, str, open, close+1, max);
+            str.resize(str.size() - 1);
+        }
+    }
+    vector<string> generateParenthesis(int n) {
+        vector<string> ans;
+        string tmp = "";
+        gen(ans, tmp, 0, 0, n);
+        return ans;
+    }
+};
