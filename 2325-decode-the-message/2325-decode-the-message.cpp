b3513595604792524c078d1cea5800017b696b02
Bhavesh Malhotra <48746746+CodeBulletin@users.noreply.github.com>
2022-10-28 20:18:49 +0530

    Time: 12 ms (29.42%), Space: 7.2 MB (24.45%) - LeetHub

---
+++ 2325-decode-the-message/2325-decode-the-message.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string decodeMessage(string key, string message) {
+        unordered_map<char, char> keymap;
+        int i = 0;
+        for(const char& k : key) {
+            if(k != ' ' && keymap.find(k) == keymap.end()) {
+                keymap[k] = i + 'a';
+                i++;
+            }
+        }
+        keymap[' '] = ' ';
+        
+        string str;
+        for(const char& c: message) {
+            str += keymap[c];
+        }
+        
+        return str;
+    }
+};
