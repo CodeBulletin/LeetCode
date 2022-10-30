780e2cbd2d39c1e482b80f15f2b8bb7d528bed42
Bhavesh Malhotra <48746746+CodeBulletin@users.noreply.github.com>
2022-10-30 11:15:02 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (80.39%) - LeetHub

--- 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.cpp
+++ 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
-    int xorOperation(int n, int start) {
+    int xorOperation(const int& n, const int& start) {
         int Xor = start;
         for(int i = 1; i < n; i++) {
-            Xor = Xor xor (start + 2 * i);
+            Xor ^= (start + 2 * i);
         }
         return Xor;
     }
